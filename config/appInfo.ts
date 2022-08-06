99430ad8dc78ba57bf456fad2b959577dd4720ef
Darron Jennings <darronj@outlook.com>
2022-08-06 14:53:32 -0500

    updating for url

--- config/appInfo.ts
+++ config/appInfo.ts
@@ -3,7 +3,8 @@ const port = process.env.PORT || 3000;
 const apiBasePath = '/api/auth/';
 
 export const websiteDomain =
-  process.env.NEXT_PUBLIC_VERCEL_URL ||
+  process.env.NEXT_PUBLIC_URL ||
+  process.env.VERCEL_URL ||
   process.env.APP_URL ||
   process.env.NEXT_PUBLIC_APP_URL ||
   `http://localhost:${port}`;
