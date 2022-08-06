075957d0b85d5d490ce13d7c886b280181359b12
Darron Jennings <darronj@outlook.com>
2022-08-06 14:41:24 -0500

    test with vercel public url

--- config/appInfo.ts
+++ config/appInfo.ts
@@ -1,9 +1,9 @@
-const port = process.env.APP_PORT || 3000;
+const port = process.env.PORT || 3000;
 
 const apiBasePath = '/api/auth/';
 
 export const websiteDomain =
-  process.env.VERCEL_URL ||
+  process.env.NEXT_PUBLIC_VERCEL_URL ||
   process.env.APP_URL ||
   process.env.NEXT_PUBLIC_APP_URL ||
   `http://localhost:${port}`;
