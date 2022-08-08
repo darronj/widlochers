0cbfcf205837484366df31b56c9d2231c8eebeec
Darron Jennings <darronj@outlook.com>
2022-08-07 22:27:23 -0500

    fixed build error

--- pages/auth/[[...path]].tsx
+++ pages/auth/[[...path]].tsx
@@ -1,32 +1,30 @@
-import Head from 'next/head'
-import React, { useEffect } from 'react'
-import styles from '../../styles/Home.module.css'
-import dynamic from 'next/dynamic'
-import SuperTokens from 'supertokens-auth-react'
-import { redirectToAuth } from 'supertokens-auth-react/recipe/thirdpartyemailpassword'
+import { Container } from '@mantine/core';
+import dynamic from 'next/dynamic';
+import Head from 'next/head';
+import { useEffect } from 'react';
+import SuperTokens from 'supertokens-auth-react';
+import { redirectToAuth } from 'supertokens-auth-react/recipe/thirdpartyemailpassword';
 
 const SuperTokensComponentNoSSR = dynamic(
   new Promise((res) => res(SuperTokens.getRoutingComponent)),
   { ssr: false }
-)
+);
 
 export default function Auth() {
   useEffect(() => {
     if (SuperTokens.canHandleRoute() === false) {
-      redirectToAuth()
+      redirectToAuth();
     }
-  }, [])
+  }, []);
 
   return (
-    <div className={styles.container}>
+    <Container>
       <Head>
         <title>SuperTokens 💫</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <SuperTokensComponentNoSSR />
-      </main>
-    </div>
-  )
+      <SuperTokensComponentNoSSR />
+    </Container>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,8 @@
-import { Title } from '@mantine/core';
 import ComingSoon from '../components/ComingSoon/ComingSoon';
 
 export default function Home() {
   return (
     <>
-      <Title>Make Me White</Title>
       <ComingSoon />;
     </>
   );
